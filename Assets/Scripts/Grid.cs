d1f2ab3bb49eb463826d888df28177cbfc9a7ceb
Jarskih <madcook86@gmail.com>
2020-01-21 11:31:41 +0100

    Init

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    class Node {
+        int x,y,z;
+        private List<Entity> _entities;
+    }
+
+    private List<Node> _nodes;
+    private int _gridSizeX = 50;
+    private int _gridSizeY = 1;
+    private int _gridSizeZ = 50;
+
+    private int _nodeSizeX = 2;
+    private int _nodeSizeZ = 2;
+}
